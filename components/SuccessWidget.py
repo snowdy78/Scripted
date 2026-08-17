a4f48e8a3ceed7d86e995949278c0386378cb4f8
snowdy78 <themrleoner12@mail.ru>
2026-08-17 18:58:50 +0700

    remove success widget

--- components/SuccessWidget.py
+++
@@ -1,8 +0,0 @@
-from PySide6.QtWidgets import QWidget
-from PySide6.QtSvg import QSvgRenderer
-
-class SuccessWidget(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.renderer = QSvgRenderer("icons/success.svg")
-        self.setFixedSize(self.renderer.defaultSize())
